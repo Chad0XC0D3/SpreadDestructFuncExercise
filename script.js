372db76c24e2624469d25d1e66960910d046e899
Student <chadharris2002@gmail.com>
2022-11-01 10:34:10 -0400

    We did it

---
+++ script.js
@@ -0,0 +1,51 @@
+// 1a
+
+const jPDinos = ["Velociraptors", "Tyrannosaurus Rex", "Dilophosaurus"];
+
+// 2a
+function seeDinos(a, ...b) {
+    console.log(a);
+    console.log(b);
+}
+
+seeDinos(...jPDinos);
+
+// 2a
+const jpCharacters = {
+    alanGrant: "Sam Neill",
+    ellieSattler: "Laura Dern",
+    ianMalcolm: "Jeff Goldblum"
+}
+
+// 2b
+seeCharacters = ({ellieSattler}) => {
+    console.log(ellieSattler);
+}
+
+seeCharacters(jpCharacters);
+
+// 3a
+
+const jurassicParkMovies = [
+    {
+        one: "Jurassic Park",
+        two: "The Lost World: Jurassic Park",
+        three: "Jurassic Park III"
+    },
+    {
+        four: "Jurassic World", 
+        five: "Jurassic World: Fallen Kingdom", 
+        six: "Jurassic World: Dominion"
+    }
+];
+
+const seeJPMovies = function ([{one,two,three},{four,five,six}]) {
+    console.log(one);
+    console.log(two);
+    console.log(three);
+    console.log(four);
+    console.log(five);
+    console.log(six);
+}
+
+seeJPMovies(jurassicParkMovies);
